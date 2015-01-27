2cd44578a3901c19db8d595271e0f543d23ab756
Max Li <max.li@live.com>
2015-01-27 15:08:25 -0600

    passport support.

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
+var flash = require('connect-flash');
+var passport = require('passport');
 
 // Setup Routes
 var routes    = require('./server/routes/index');
@@ -25,6 +26,9 @@ mongoose.connection.on('error', function() {
 
 var app = express();
 
+require('./server/config/passport') (passport);
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'ejs');
@@ -40,6 +44,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/api/speakers', speakers);
+app.use(flash());
+// Init passport authentication
+app.use(passport.initialize());
+// persistent login sessions
+app.use(passport.session());
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
